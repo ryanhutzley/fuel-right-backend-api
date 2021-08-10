991ea5abaae26827cd9d3deceecbef24cf12ac9d
Ryan Hutzley <ryanhutzley@gmail.com>
2021-08-10 17:04:34 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+end
