0659fc61eeeef36147a7e3f17a34893399508871
Ryan Hutzley <ryanhutzley@gmail.com>
2021-08-12 18:49:07 -0400

    fonts, navbar, partially complete login

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,6 +1,62 @@
+import { Form, Button } from 'react-bootstrap'
+
 function Login() {
     return (
-        <div></div>
+        <>
+            <div style={{display: 'flex', alignItems:'center', flexDirection: 'column', textAlign: 'center', width: '80%', margin: 'auto'}}>
+                <h1 style={{marginTop: '50px', textAlign: 'center'}}>Welcome to <span id="brand" style={{textDecorationLine: 'underline'}}>FuelRight</span></h1>
+                <br></br>
+                <div style={{fontStyle: 'italic'}}>
+                    <h2>A digital lifestyle log so you can optimize your performance the arena of your choosing</h2>
+                    <br></br>
+                    <h2>Unlocking a better you is requires diligence and careful monitoring</h2>
+                    <br></br>
+                    <h2><span id="brand" style={{textDecorationLine: 'underline', fontStyle: 'normal'}}>FuelRight</span> makes it simple</h2>
+                </div>
+            </div>
+            <section className="vh-100 gradient-custom">
+                <div className="container py-5 h-100">
+                    <div className="row justify-content-center align-items-center h-100">
+                        <div className="col-12 col-lg-9 col-xl-7">
+                            <div className="card shadow-2-strong card-registration" style={{borderRadius: "15px"}}>
+                                <div className="card-body p-4 p-md-5">
+                                    <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">FuelRight Signup</h3>
+                                    <Form>
+                                        <Form.Group className="mb-3" controlId="formBasicName">
+                                            <Form.Label>Name</Form.Label>
+                                            <Form.Control type="name" placeholder="Enter name" />
+                                        </Form.Group>
+                                        
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control type="email" placeholder="Enter email" />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control type="password" placeholder="Password" />
+                                        </Form.Group>
+                                        
+                                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                                            <Form.Label>Password Confirmation</Form.Label>
+                                            <Form.Control type="password" placeholder="Password" />
+                                        </Form.Group>
+                                        
+                                        <Form.Group className="mb-3" controlId="formBasicWeight">
+                                            <Form.Label>Weight</Form.Label>
+                                            <Form.Control type="number" placeholder="Weight in lbs." />
+                                        </Form.Group>
+                                        <Button variant="primary" type="submit">
+                                            Create Account
+                                        </Button>
+                                    </Form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -4,13 +4,13 @@ function NavBar() {
     return (
         <Navbar bg="dark" variant="dark" sticky="top">
             <Container>
-            <Navbar.Brand>FuelRight</Navbar.Brand>
+            <Navbar.Brand id="brand">FuelRight</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link to="/login">Login</Nav.Link>
                     <Nav.Link to="/">Your Tracker</Nav.Link>
                     <Nav.Link to="/day">Your Day</Nav.Link>
                     <Nav.Link to="/history">Your History</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown title="Your Profile" id="basic-nav-dropdown">
                         <NavDropdown.Item to="/edit">Edit Profile</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item to="/login">Logout</NavDropdown.Item>
