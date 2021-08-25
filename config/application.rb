8afaec4249d85a35a35705f15d73c0723bd5d8ac
Ryan Hutzley <ryanhutzley@gmail.com>
2021-08-25 18:06:29 -0400

    config.time_zone = 'UTC'

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module FuelRightBackendApi
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Eastern Time (US & Canada)"
+    config.time_zone = "UTC"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
